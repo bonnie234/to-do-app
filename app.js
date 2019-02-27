2029b33aa50784c188154e688c473eccd41facd0
Bonnie Gabrielson <bonniegabrielson@gmail.com>
2019-02-26 20:20:22 -0600

    to-do-app Dom2 assignment update

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 
 function onReady() {
-  let id = 0;
-  {
+    let id = 0;
+    const toDos = [];
 
-  console.log(x);
+    const addToDoForm = document.getElementById('addToDoForm');
 
-}
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
 
-
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
-
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    let minusBtn = document.createElement('button');
-    minusBtn.innerHTML = '<span>Delete</span>';;
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the checkbox to the li
-    newLi.appendChild(minusBtn);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
+    toDos.push({
+    title: newToDoText.value,
+    complete: false,
+    id: ++id
+  });
     newToDoText.value = '';
 
-minusBtn.addEventListener('click', function() {
-   newLi.parentNode.removeChild(newLi);
-})
+    renderTheUI();
+  }
+}
 
-var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
 
+    const deleteBtn = document.createElement('button')
+         deleteBtn.innerHTML = '<span>Delete</span>';
+
+
+    newLi.innerHTML = toDo.title;
+    newLi.appendChild(deleteBtn);
+  });
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+
+/ var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 const result = words.filter(word => word.length > 6);
+console.log(result); //
+    });
+ }
 
-console.log(title);
-   });
-};
+  addToDoForm.addEventListener('submit', event => {
+  event.preventDefault();
+  createNewToDo();
+});
+
+  renderTheUI();
+}
 
 window.onload = function() {
-   onReady();
- };
+  onReady();
+};
